78ae7be6717ee5ba1569974094a3215c7ab94f84
MYSEY <52909452+MYSEY@users.noreply.github.com>
2024-04-18 15:40:33 +0700

    create new function issue type

--- app/Http/Controllers/Admins/IssueTypeController.php
+++ app/Http/Controllers/Admins/IssueTypeController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Admins;
 
-use App\Http\Controllers\Controller;
+use App\Models\IssueType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Brian2694\Toastr\Facades\Toastr;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\IssueTypeRequest;
 
 class IssueTypeController extends Controller
 {
@@ -12,7 +17,8 @@ class IssueTypeController extends Controller
      */
     public function index()
     {
-        return view('issue_type.index');
+        $data = IssueType::all();
+        return view('issue_type.index',compact('data'));
     }
 
     /**
@@ -26,17 +32,31 @@ class IssueTypeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(IssueTypeRequest $request)
     {
-        //
+        try {
+            $data = $request->all();
+            $data['created_by'] = Auth::user()->id;
+            IssueType::create($data);
+            DB::commit();
+            Toastr::success('Created Issue Type successfully.','Success');
+            return redirect()->back();
+        } catch (\Throwable $exp) {
+            DB::rollback();
+            Toastr::error('Created Issue Type fail','Error');
+            return redirect()->back();
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Request $request)
     {
-        //
+        $data = IssueType::where('id',$request->id)->first();
+        return response()->json([
+            'success'=>$data,
+        ]);
     }
 
     /**
@@ -52,14 +72,34 @@ class IssueTypeController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            IssueType::where('id',$request->id)->update([
+                'name'    => $request->name,
+                'updated_by'    => Auth::user()->id,
+            ]);
+            DB::commit();
+            Toastr::success('Updated Issue Type successfully.','Success');
+            return redirect()->back();
+        } catch (\Throwable $exp) {
+            DB::rollback();
+            Toastr::error('Updated Issue Type fail','Error');
+            return redirect()->back();
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request)
     {
-        //
+        try{
+            IssueType::destroy($request->id);
+            Toastr::success('Issue Type deleted successfully.','Success');
+            return redirect()->back();
+        }catch(\Exception $e){
+            DB::rollback();
+            Toastr::error('Issue Type delete fail.','Error');
+            return redirect()->back();
+        }
     }
 }
--- resources/views/issue_type/index.blade.php
+++ resources/views/issue_type/index.blade.php
@@ -12,7 +12,7 @@
             <div class="panel-container show">
                 <div class="panel-tag">
                     <div class="text-lg-right">
-                        <button class="btn btn-success btn-sm mr-1" data-toggle="modal" data-target="#issue-create" type="button"><span><i class="fal fa-plus mr-1"></i> Add New</span></button>
+                        <button class="btn btn-success btn-sm mr-1" data-toggle="modal" data-target="#issue-type-create" type="button"><span><i class="fal fa-plus mr-1"></i> Add New</span></button>
                     </div>
                 </div>
                 <div class="panel-content">
@@ -22,26 +22,24 @@
                             <tr>
                                 <th>ID</th>
                                 <th>Name</th>
-                                <th>Color</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {{-- @if (count($data)>0)
+                            @if (count($data)>0)
                                 @foreach ($data as $key=>$item)
                                     <tr>
                                         <td class="ids">{{$item->id}}</td>
                                         <td class="name">{{$item->name}}</td>
-                                        <td class="color" style="color: {{$item->color}}">{{$item->name}}</td>
                                         <td>
                                             <div class="d-flex demo">
-                                                <a href="javascript:void(0);" class="btn btn-sm btn-outline-danger btn-icon btn-inline-block mr-1 priorityDelete" data-toggle="modal" data-target="#priority_status" data-id="{{$item->id}}" title="Delete Record"><i class="fal fa-times"></i></a>
+                                                <a href="javascript:void(0);" class="btn btn-sm btn-outline-danger btn-icon btn-inline-block mr-1 IssueTypeDelete" data-toggle="modal" data-target="#issue_type" data-id="{{$item->id}}" title="Delete Record"><i class="fal fa-times"></i></a>
                                                 <a class="btn btn-sm btn-outline-primary btn-icon btn-inline-block mr-1 update" data-id="{{$item->id}}" title="Edit"><i class="fal fa-edit"></i></a>
                                             </div>
                                         </td>
                                     </tr>
                                 @endforeach
-                            @endif --}}
+                            @endif
                         </tbody>
                     </table>
                 </div>
@@ -50,8 +48,8 @@
     </div>
 </div>
 
-<!-- Modal Create New Priority -->
-<div class="modal custom-modal fade" id="issue-create" role="dialog" aria-hidden="true" data-bs-backdrop="static">
+<!-- Modal Create New Issue Type -->
+<div class="modal custom-modal fade" id="issue-type-create" role="dialog" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -65,7 +63,7 @@
                     @csrf
                     <div class="form-group">
                         <label class="form-label">Name <span class="text-danger">*</span></label>
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name">
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{old('name')}}">
                     </div>
                     <div class="float-lg-right">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -77,7 +75,7 @@
     </div>
 </div>
 
-<!-- Modal Edit Priority -->
+<!-- Modal Edit Issue Type -->
 <div class="modal fade" id="editIssueType" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -88,13 +86,13 @@
                 </button>
             </div>
             <div class="modal-body">
-                <form action="{{url('admin/issue-type/update')}}" method="POST" enctype="multipart/form-data">
+                <form action="{{url('admin/issue-type/update')}}" method="POST" enctype="multipart/form-data" class="needs-validation" novalidate>
                     @csrf
                     @method('PUT')
                     <input type="hidden" name="id" class="e_id" id="e_id" value="">
                     <div class="form-group">
                         <label class="form-label">Name <span class="text-danger">*</span></label>
-                        <input type="text" id="e_name" class="form-control @error('name') is-invalid @enderror" name="name">
+                        <input type="text" id="e_name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{old('name')}}">
                     </div>
                     <div class="float-lg-right">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -107,7 +105,7 @@
 </div>
 
 <!-- Delete Department Modal -->
-<div class="modal custom-modal fade" id="priority_status" role="dialog">
+<div class="modal custom-modal fade" id="issue_type" role="dialog">
     <div class="modal-dialog modal-sm modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-body">
@@ -116,7 +114,7 @@
                     <p>Are you sure want to delete?</p>
                 </div>
                 <div class="modal-btn delete-action">
-                    <form action="{{url('admin/priority/delete')}}" method="POST" enctype="multipart/form-data">
+                    <form action="{{url('admin/issue-type/delete')}}" method="POST" enctype="multipart/form-data">
                         @csrf
                         @method("DELETE")
                         <input type="hidden" name="id" class="e_id" id="e_id" value="">
@@ -139,7 +137,7 @@
                 let id = $(this).data("id");
                 $.ajax({
                     type: "GET",
-                    url: "{{url('admin/priority/edit')}}",
+                    url: "{{url('admin/issue-type/edit')}}",
                     data: {
                         id : id
                     },
@@ -148,13 +146,12 @@
                         if (response.success) {
                             $('#e_id').val(response.success.id);
                             $('#e_name').val(response.success.name);
-                            $('#e_color').val(response.success.color);
                             $('#editIssueType').modal('show');
                         }
                     }
                 });
             });
-            $(".priorityDelete").on('click',function(){
+            $(".IssueTypeDelete").on('click',function(){
                 $('.e_id').val($(this).data("id"));
             });
         });
