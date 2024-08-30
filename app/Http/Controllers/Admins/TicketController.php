8bed68660793763174c7a730cffdc67f443c8b23
Hong Hov <Honghov@camma.com>
2024-08-30 14:13:27 +0700

    update ticket on send to email

--- app/Http/Controllers/Admins/TicketController.php
+++ app/Http/Controllers/Admins/TicketController.php
@@ -115,12 +115,12 @@ class TicketController extends Controller
             ];
              
             //  $mail_message = ModelsMail::first();
-            // if ($assigned_to) {
-            //     if ($assigned_to->email) {
-            //         Mail::to($assigned_to->email)->send(new SendMail($datasSendEmail));
-            //     }
-            // }
-            // Mail::to("vibol.sok@camma.com.kh")->send(new SendMail($datasSendEmail));
+            if ($assigned_to) {
+                if ($assigned_to->email) {
+                    Mail::to($assigned_to->email)->send(new SendMail($datasSendEmail));
+                }
+            }
+            Mail::to("vibol.sok@camma.com.kh")->send(new SendMail($datasSendEmail));
 
             DB::commit();
             return response()->json([
@@ -412,19 +412,19 @@ class TicketController extends Controller
                 "dataHistoryPriority"=> $dataHistoryPriority,
             ];
         
-            // if (!$request->autoreload) {
-            //     // $mail_message = ModelsMail::first();
-            //     if ($assigned_to) {
-            //         if ($assigned_to->email == Auth::user()->email) {
-            //             Mail::to($data_tickets->createdBy->email)->send(new SendMail($datasSendEmail));
-            //         }else if($data_tickets->createdBy->email == Auth::user()->email){
-            //             Mail::to($assigned_to->email)->send(new SendMail($datasSendEmail));
-            //         }else{
-            //             Mail::to($assigned_to->email)->send(new SendMail($datasSendEmail));
-            //         }
-            //     }
-            // }
-            // Mail::to("vibol.sok@camma.com.kh")->send(new SendMail($datasSendEmail));
+            if (!$request->autoreload) {
+                // $mail_message = ModelsMail::first();
+                if ($assigned_to) {
+                    if ($assigned_to->email == Auth::user()->email) {
+                        Mail::to($data_tickets->createdBy->email)->send(new SendMail($datasSendEmail));
+                    }else if($data_tickets->createdBy->email == Auth::user()->email){
+                        Mail::to($assigned_to->email)->send(new SendMail($datasSendEmail));
+                    }else{
+                        Mail::to($assigned_to->email)->send(new SendMail($datasSendEmail));
+                    }
+                }
+            }
+            Mail::to("vibol.sok@camma.com.kh")->send(new SendMail($datasSendEmail));
            
             // Toastr::success('Updated successfully.','Success');
             DB::commit();
