5cd13b22f3b8ec8c4d32679452248bd111db5698
MYSEY <52909452+MYSEY@users.noreply.github.com>
2024-12-09 14:34:29 +0700

    update generate ticketid

--- app/Traits/GeneratingTicketID.php
+++ app/Traits/GeneratingTicketID.php
@@ -14,24 +14,27 @@ trait GeneratingTicketID
     {
         $currentYear = Carbon::now();
         $count = 0;
-        $geticketID = Ticket::orderBy('trackid','DESC')->select('trackid')->get();
+        $geticketID = Ticket::orderBy('id','DESC')->select('trackid')->limit(4)->get();
         if (!empty($geticketID)) {
             for ($i = 0; $i < count($geticketID); $i++) {
-                $current = (int) substr(strrchr($geticketID[$i]->trackid, "-"), 1);
+                // $current = $geticketID[$i]->trackid;
+                $current = (int) substr($geticketID[$i]->trackid,1);
                 if ($i + 1 < count($geticketID)) {
-                    $next = (int) substr(strrchr($geticketID[$i + 1]->trackid, "-"), 1);
+                    $next = (int) substr($geticketID[$i + 1]->trackid,1);
                 }
                 
                 if (isset($next) && $current + 1 != $next) {
-                    $count = (int) substr(strrchr($geticketID[$i]->trackid, "-"), 1);
+                    $count = (int) substr($geticketID[$i]->trackid,1);
+                    // $count = (int) substr(strrchr($geticketID[$i]->trackid, "0"), 1);
                     break;
                 } else {
-                    $count = (int) substr(strrchr($geticketID[$i]->trackid, "-"), 1);
+                    // $count = (int) substr($geticketID[$i]->trackid,1);
+                    $count = (int) substr($geticketID[$i]->trackid,1);
                 }
             }
         }
         do {
-            $ticketID = $currentYear->format('y').'-'.str_pad(($count + 1), 6, "0", STR_PAD_LEFT);
+            $ticketID = $currentYear->format('y').'0'.str_pad(($count+ 1), 6, "0", STR_PAD_LEFT);
             $alreadyExist = Ticket::select('trackid')->where('trackid', $ticketID)->first()->trackid ?? null;
             $count++;
         } while ($alreadyExist);
