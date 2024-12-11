dc791cde0172105488a3feeb8ca5734195761816
MYSEY <52909452+MYSEY@users.noreply.github.com>
2024-12-11 11:55:03 +0700

    update ticke generate

--- app/Traits/GeneratingTicketID.php
+++ app/Traits/GeneratingTicketID.php
@@ -14,22 +14,22 @@ trait GeneratingTicketID
     {
         $currentYear = Carbon::now();
         $count = 0;
-        $geticketID = Ticket::orderBy('id','DESC')->select('trackid')->limit(4)->get();
+        $geticketID = Ticket::orderBy('id','DESC')->select('trackid')->get();
         if (!empty($geticketID)) {
             for ($i = 0; $i < count($geticketID); $i++) {
                 // $current = $geticketID[$i]->trackid;
-                $current = (int) substr($geticketID[$i]->trackid,1);
+                $current = (int) substr($geticketID[$i]->trackid,2);
                 if ($i + 1 < count($geticketID)) {
-                    $next = (int) substr($geticketID[$i + 1]->trackid,1);
+                    $next = (int) substr($geticketID[$i + 1]->trackid,2);
                 }
                 
                 if (isset($next) && $current + 1 != $next) {
-                    $count = (int) substr($geticketID[$i]->trackid,1);
+                    $count = (int) substr($geticketID[$i]->trackid,2);
                     // $count = (int) substr(strrchr($geticketID[$i]->trackid, "0"), 1);
                     break;
                 } else {
                     // $count = (int) substr($geticketID[$i]->trackid,1);
-                    $count = (int) substr($geticketID[$i]->trackid,1);
+                    $count = (int) substr($geticketID[$i]->trackid,2);
                 }
             }
         }
