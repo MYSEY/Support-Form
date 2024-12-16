b9f610b754a34be510e07af544ef81c499bf0471
MYSEY <52909452+MYSEY@users.noreply.github.com>
2024-12-16 16:06:16 +0700

    update generat

--- app/Traits/GeneratingTicketID.php
+++ app/Traits/GeneratingTicketID.php
@@ -18,23 +18,23 @@ trait GeneratingTicketID
         if (!empty($geticketID)) {
             for ($i = 0; $i < count($geticketID); $i++) {
                 // $current = $geticketID[$i]->trackid;
-                $current = (int) substr($geticketID[$i]->trackid,2);
+                $current = (int) substr($geticketID[$i]->trackid,4);
                 if ($i + 1 < count($geticketID)) {
-                    $next = (int) substr($geticketID[$i + 1]->trackid,2);
+                    $next = (int) substr($geticketID[$i + 1]->trackid,4);
                 }
                 
                 if (isset($next) && $current + 1 != $next) {
-                    $count = (int) substr($geticketID[$i]->trackid,2);
+                    $count = (int) substr($geticketID[$i]->trackid,4);
                     // $count = (int) substr(strrchr($geticketID[$i]->trackid, "0"), 1);
                     break;
                 } else {
                     // $count = (int) substr($geticketID[$i]->trackid,1);
-                    $count = (int) substr($geticketID[$i]->trackid,2);
+                    $count = (int) substr($geticketID[$i]->trackid,4);
                 }
             }
         }
         do {
-            $ticketID = $currentYear->format('y').'0'.str_pad(($count+ 1), 6, "0", STR_PAD_LEFT);
+            $ticketID = $currentYear->format('ymd').str_pad(($count+ 1), 2, "0", STR_PAD_LEFT);
             $alreadyExist = Ticket::select('trackid')->where('trackid', $ticketID)->first()->trackid ?? null;
             $count++;
         } while ($alreadyExist);
