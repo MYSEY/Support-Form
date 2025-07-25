5abb7e19400a5b8c5d663dac5efdbe913dd11020
MYSEY <52909452+MYSEY@users.noreply.github.com>
2025-07-25 13:48:47 +0700

    update asset

--- resources/views/asset/index.blade.php
+++ resources/views/asset/index.blade.php
@@ -51,7 +51,7 @@
                             <div class="text-lg-right">
                                 @can('Asset Import')
                                     <a type="button" id="btn-import" href="#" data-toggle="modal" data-target="#modal-import" class="btn btn-danger btn-sm mr-1"><i class="fal fa-file"></i> Import</a>
-                                    <a href="javascript:void(0)" class="btn btn-outline-success waves-effect waves-themed mr-1" id="btn-export" tabindex="0" aria-controls="dt-basic-example" type="button" title="Generate Excel"><span>Excel</span></a>
+                                    <a href="javascript:void(0)" class="btn btn-outline-success btn-sm waves-effect waves-themed mr-1" id="btn-export" tabindex="0" aria-controls="dt-basic-example" type="button" title="Generate Excel"><span>Excel</span></a>
                                 @endcan
                                 <a href="{{url('admin/asset/create')}}" class="btn btn-success btn-sm mr-1"><span><i class="fal fa-plus mr-1"></i> Add New</span></a>
                             </div>
